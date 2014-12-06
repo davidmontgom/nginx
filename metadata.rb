20f97a8335f3cd75a05ee7d3c1d690b345651790
David Montgomery <davidmontgomery@gmail.com>
2014-12-06 18:26:02 +0800

    test

--- metadata.rb
+++ metadata.rb
@@ -1,16 +1,8 @@
-<<<<<<< HEAD
 name             "nginx"
 maintainer       "Rtb"
 maintainer_email "nobdy"
 license          "Apache 2.0"
 description      "Installs nginx"
-=======
-name             "uWSGI"
-maintainer       "Rtb"
-maintainer_email "nobdy"
-license          "Apache 2.0"
-description      "Installs uWSGI"
->>>>>>> d563bb24c29d04aedfea62d7706cb18f7a1f6986
 version          "1.0.0"
 %w{ubuntu}.each do |os|
   supports os

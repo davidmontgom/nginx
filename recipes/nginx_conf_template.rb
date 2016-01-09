39916af8f5cf102e6c2ab706153bc99c75735e62
David Montgomery <davidmontgomery@gmail.com>
2016-01-09 22:51:59 +0800

    f

--- recipes/nginx_conf_template.rb
+++ recipes/nginx_conf_template.rb
@@ -1,17 +1,8 @@
-data_bag("my_data_bag")
-db = data_bag_item("my_data_bag", "my")
-#location = node.name.split('-')[2]
-
-if node.name.include? "-"
-  datacenter = node.name.split('-')[0]
-  server_type = node.name.split('-')[1]
-  location = node.name.split('-')[2]
-end
-if node.name.include? "X"
-  datacenter = node.name.split('X')[0]
-  server_type = node.name.split('X')[1]
-  location = node.name.split('X')[2]
-end
+datacenter = node.name.split('-')[0]
+environment = node.name.split('-')[1]
+location = node.name.split('-')[2]
+server_type = node.name.split('-')[3]
+slug = node.name.split('-')[4] 
 
 service "nginx" do
   supports :restart => true, :start => true, :stop => true, :reload => true
